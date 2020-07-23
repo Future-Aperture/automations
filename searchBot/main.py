984131e4557b91bb957ef3a34dd37763f830c604
Miguel-Perfume <miguelpafume@hotmail.com>
2020-07-23 14:53:17 -0300

    adicionado alguns comentarios, e limite de buscas

--- searchBot/main.py
+++ searchBot/main.py
@@ -2,10 +2,12 @@ from lxml import html
 from googlesearch import search
 import requests, re, functions
 
-# <---------------------||-------------------->
+# <---------------------| Variaveis Globais |-------------------->
 
-listaPreco = [] # Preços
-sites = [] # Sites que serão usados
+# Exibição dos sites no txt
+sites = ["Mercado Livre", "Kabum"]
+
+# Dict para ser usado nas buscas
 dictSites = {   'Mercado Livre 1': 'produto.mercadolivre.com.br',
                 'Mercado Livre 2': 'www.mercadolivre.com.br',
                 'Mercado Livre 3': 'lista.mercadolivre.com.br',
@@ -13,12 +15,22 @@ dictSites = {   'Mercado Livre 1': 'produto.mercadolivre.com.br',
                 'Mercado Livre 5': 'loja.mercadolivre.com.br',
                 'Kabum': 'www.kabum.com.br'}
 
-# <---------------------||-------------------->
+# <---------------------| Sites Usados|-------------------->
 
+# O que vai buscar no google
 busca = input("Digite o que você quer buscar:\n> ")
 
+while True:
+    try:
+        limite = int(input("\nQuantos links você deseja adquirir?\n> "))
+        break
+    except ValueError:
+        print("Tente novamente.")
+    
+# Pega os links da busca
 links = search(busca, num = 10, start = 0, stop = 10, pause = 2)
 
+# Filtra os sites de acordo com o <dictSites>
 sites = functions.sitesFuncionais(links, dictSites)
 
 # <---------------------||-------------------->
